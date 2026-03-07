f4bc618bab653b7ebf34afc8a4fd6630064b6b8c
Colbee Thrasher <colbeethr@gmail.com>
2026-03-07 16:51:04 -0600

    finished makeing commits and fixed spelling.

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -13,13 +13,13 @@ namespace APIsAndJSON
             //Build an api url from where the api will call from.
             var kanyeUrl = "https://api.kanye.rest";
             
-            //useing the HTTPClient instance
+            //using the HTTPClient instance
             //Send a git request to the URL created above. this will give us back a string of Json
             var kanyeResponseJson = client.GetStringAsync(kanyeUrl).Result;
             
             //now we parse the JSON response string into JObject
             //we do this to access certain parts of the JSON
-            //in this case er will getting the value of the key called "quote" witch will give us the actual quote itself
+            //in this case we will get the value of the key called "quote" witch will give us the actual quote itself
             // - not the JSON body
             
            //      var kanyeQuote = JObject.Parse(kanyeResponseJson).GetValue("quote").ToString();
@@ -31,9 +31,16 @@ namespace APIsAndJSON
            
            
            //Ron
+           // Create an instance of a class "HTTPClient" . This is what will make out api call
            var clientTwo = new HttpClient();
+           
+           //Build an api url from where the api will call from.
            var ronUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+           
+           //using the HTTPClient instance
+           //Send a git request to the URL created above. this will give us back a string of Json
            var ronResponseJson = client.GetStringAsync(ronUrl).Result;
+           
                // we use JArray so that the api returns a JSON array
            var ronQuote = JArray.Parse(ronResponseJson)[0].ToString();
            Console.WriteLine(ronQuote);
